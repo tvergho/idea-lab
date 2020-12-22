e68529ae52c4f596533e0373121000137d4c437b
tvergho <tvergho@gmail.com>
2020-12-21 18:52:37 -0800

    See all button

---
+++ components/Posts/PostBlock/assets.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ForwardArrow = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6.38501 16.5901L11.5 12.0001L6.5 7.5001L7.08997 6.79565L13 12.0001L10.295 14.5001L7.08997 17.2951L6.38501 16.5901Z" fill="white" />
+    </svg>
+  );
+};
--- components/Posts/PostBlock/index.js
+++ components/Posts/PostBlock/index.js
@@ -1,7 +1,11 @@
-import React, {useContext} from 'react';
-import PostGrid from '../PostGrid';
+import React, { useContext } from 'react';
 import GlobalContext from 'context/GlobalContext';
+import Link from 'next/link';
+import PostGrid from '../PostGrid';
+import { ForwardArrow } from './assets';
 import styles from './styles.module.scss';
+
+const UPDATES_PAGE = '/updates';
 
 const PostBlock = () => {
   const { recentPosts } = useContext(GlobalContext);
@@ -10,6 +14,13 @@ const PostBlock = () => {
     <div className={styles['post-container']}>
       <div className={styles.header}>
         <h3>Press Releases</h3>
+
+        <Link href={UPDATES_PAGE}>
+          <button className={`transparent opacity-toggle ${styles['see-all']}`} type="button">
+            <span>See all</span>
+            <ForwardArrow />
+          </button>
+        </Link>
       </div>
       <PostGrid posts={recentPosts} />
     </div>
