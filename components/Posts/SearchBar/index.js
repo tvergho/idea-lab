b1c62813d52df45743c8a954146bbc9541b2205b
tvergho <tvergho@gmail.com>
2020-12-22 21:46:11 -0800

    Bottom border on PostGrid

---
+++ components/Posts/SearchBar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
+
+const SearchBar = () => {
+  return (
+    <div />
+  );
+};
+
+export default SearchBar;
